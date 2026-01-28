7ec041b2c58f5b3e11605e3b015d20db71b38c71
Bennett Buckley <ben.buckley@outlook.com>
2026-01-27 23:06:13 -0500

    refactor: Use arg groups for target choice

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod api;
 use std::{error::Error, fmt::Display, io::Write};
 
 use api::Api;
-use clap::{Parser, ValueEnum};
+use clap::{Args, Parser, ValueEnum};
 use serde::{Deserialize, Serialize};
 
 const CHAR_REPLACE: &[[&str; 2]] = &[
@@ -61,26 +61,33 @@ async fn get_id_from_user() -> Option<u64> {
 async fn do_search(matches: Cli, config: Config) -> Result<(), Box<dyn Error>> {
     let api = Api::new(&config.api_key).await?;
 
-    let target_series: u64 = if matches.id.is_none() {
-        let name = matches.name.expect("Has name");
-        let series_results = api
-            .search_series(Some(&name), None, None, None, Some(&matches.lang))
-            .await?;
+    let target_series = match matches.target {
+        Target {
+            id: Some(id),
+            name: None,
+        } => id,
+        Target {
+            id: None,
+            name: Some(name),
+        } => {
+            let series_results = api
+                .search_series(Some(&name), None, None, None, Some(&matches.lang))
+                .await?;
 
-        if series_results.len() == 1 {
-            series_results[0].id
-        } else {
-            for series in series_results {
-                println!("{}: {}", series.series_name, series.id);
-            }
-            println!();
-            match get_id_from_user().await {
-                Some(id) => id,
-                None => return Ok(()), // User decided to abort
+            if series_results.len() == 1 {
+                series_results[0].id
+            } else {
+                for series in series_results {
+                    println!("{}: {}", series.series_name, series.id);
+                }
+                println!();
+                match get_id_from_user().await {
+                    Some(id) => id,
+                    None => return Ok(()), // User decided to abort
+                }
             }
         }
-    } else {
-        matches.id.expect("Should have ID")
+        _ => unimplemented!(),
     };
 
     let series = api.get_series(target_series, None).await?;
@@ -145,10 +152,8 @@ struct Cli {
     #[arg(short, long, default_value_t = Ordering::Aired)]
     /// The Episode ordering to use
     ordering: Ordering,
-    #[arg(short, long, help = "Name of a series to search for")]
-    name: Option<String>,
-    #[arg(short, long, help = "Series ID", conflicts_with = "name", value_parser = clap::value_parser!(u64).range(1..))]
-    id: Option<u64>,
+    #[command(flatten)]
+    target: Target,
     #[arg(
         short,
         long,
@@ -158,6 +163,17 @@ struct Cli {
     lang: String,
     #[arg(short, long, help = "Update configured API key")]
     key: Option<String>,
+}
+
+#[derive(Debug, Args)]
+#[group(required = true, multiple = false)]
+struct Target {
+    #[arg(short, long, value_parser = clap::value_parser!(u64).range(1..))]
+    /// Series ID
+    id: Option<u64>,
+    #[arg(short, long)]
+    /// Name of a series to search for
+    name: Option<String>,
 }
 
 static CONFIG_NAME: &str = env!("CARGO_PKG_NAME");
